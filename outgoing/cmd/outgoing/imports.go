fb297ad3b008c54bd796d3368835997fb5fa12d6
Zeal <zhengxujin@Zeal-Macbook.lan>
2017-05-05 23:19:37 +0800

    add totp confirm support

--- outgoing/cmd/outgoing/imports.go
+++ outgoing/cmd/outgoing/imports.go
@@ -2,8 +2,4 @@ package main
 
 import (
 	_ "github.com/gogap/bearychat/outgoing/triggers/auth"
-	_ "github.com/gogap/bearychat/outgoing/triggers/commands"
-	_ "github.com/gogap/bearychat/outgoing/triggers/confirm"
-	_ "github.com/gogap/bearychat/outgoing/triggers/greeter"
-	_ "github.com/gogap/bearychat/outgoing/triggers/sensitive_filter"
 )
